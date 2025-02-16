1981b5e827053ac4eb146e6d1a6e0a7c58dce104
coderadi12345 <adityasharma74304@gmail.com>
2025-02-16 22:04:04 +0530

    added

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_API } from './constant'
 import VideoCard from './VideoCard'
-
+import { Link } from 'react-router-dom'
 const VideoContainer = () => {
 
   const [videos, setVideos] = useState([])
@@ -18,8 +18,9 @@ const VideoContainer = () => {
     setVideos(json.items)
   }
   return (
-    <div>
-      <VideoCard info = {videos[0]}/>
+    <div className='flex flex-wrap'>
+     {videos.map(video =>(
+     <Link to ={"/watch?v="+video.id}><VideoCard key={video.id} info={video}/></Link>))}
     </div>
   )
 }
